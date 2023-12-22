3e0c3cf198adccc0970f5c6ab5ec59e38b45fa10
darienmiller88 <darienmiller88@yahoo.com>
2023-12-22 04:35:29 -0500

    finished adding middlewares

--- api/middlewares/ProtectSignin.go
+++ api/middlewares/ProtectSignin.go
@@ -2,14 +2,20 @@ package middlewares
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ProtectSignin(c *fiber.Ctx) error{
-	fmt.Println("path", c.Path())
-	fmt.Println(strings.HasSuffix(c.Path(), "/signin"))
+	_, err := retrieveTokenFromCookie(c.Cookies("user"))
+	pageHit := strings.Contains(c.Path(), "/signin") || strings.Contains(c.Path(), "/signup")
+
+	//If the user is currently signed in, and they try to access the signin or signup route, block the request
+	if err == nil && pageHit {
+		return c.Status(http.StatusForbidden).JSON(fiber.Map{"err": fmt.Sprintf("Cannot access %s while signed in.", c.Path())})
+	}
 	
 	return c.Next()
 }
--- api/routes/UsersRoutes.go
+++ api/routes/UsersRoutes.go
@@ -16,11 +16,11 @@ func (u *UserRoutes) Init(){
 	u.Router = fiber.New()
 	u.userController.Init()
 
-	u.Router.Use(middlewares.ProtectSignin).Post("/signin", u.userController.Signin)
-	u.Router.Post("/signup", u.userController.Signup)
 	u.Router.Post("/signout", u.userController.Signout)
+	u.Router.Use(middlewares.ProtectSignin).Post("/signin", u.userController.Signin)
+	u.Router.Use(middlewares.ProtectSignin).Post("/signup", u.userController.Signup)
+	u.Router.Use(middlewares.Auth, middlewares.ProtectUser).Get("/username/:username", u.userController.GetUserByUsername)
 	u.Router.Use(middlewares.Auth).Get("/:id", u.userController.GetUserByID)
 	u.Router.Use(middlewares.Auth).Delete("/:id", u.userController.DeleteUser)
-	u.Router.Use(middlewares.Auth).Get("/username/:username", u.userController.GetUserByUsername)
 	u.Router.Use(middlewares.Auth).Get("/", u.userController.GetUsers)
 }
