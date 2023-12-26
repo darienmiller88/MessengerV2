2bc167be0c04408219cf46cea252e4e14c3f0569
darienmiller88 <darienmiller88@yahoo.com>
2023-12-26 12:48:47 -0500

    completed message history page

--- client/src/pages/MessageHistory/MessageHistory.svelte
+++ client/src/pages/MessageHistory/MessageHistory.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
     import Sidebar from "../../components/Sidebar/Sidebar.svelte";  
-    import Message from "../../components/Message/Message.svelte";
     import { onMount } from "svelte";
     import { messageApi } from "../../api/api";
+    import { isDarkModeStore } from "../../stores"
+    import { type Message } from "../../types/type"
+
+    let messages: Message[] = []
 
     onMount(async () => {
-
+       
     })
 </script>
 
 <div class="message-history">
-    <div class="header">Message History</div>
+    <div class={$isDarkModeStore ? "header-dark-mode header" : "header"}>Message History</div>
     <div class="messages">
-        <div class="message-receipt"></div>
-        <!-- <Message username={"darienm99"} messageContent={"hi"} time={new Date().toLocaleString()} isYourMessage={true}/> -->
+        {#each messages as message}
+            <div class="message-receipt">
+                <div class="date">{message.message_date}</div>
+                <div class="username">{message.username}</div>
+                <div class="content">{message.message_content}</div>
+            </div>
+        {/each}
     </div>
     <Sidebar />
 </div>
@@ -32,9 +40,28 @@
             font-size: 35px;
         }
 
+        .header-dark-mode{
+            color: white;
+        }
+
         .messages{
             display: flex;
-            border: 2px solid red;
+            flex-direction: column;
+
+            overflow-y: scroll;
+
+            .message-receipt{
+                align-self: flex-end;
+
+                background-color: var(--messenger-blue);
+                color: white;
+
+                max-width: 70%;
+                margin: 10px;
+                padding: 15px 25px;
+                border-radius: 10px;
+                font-size: 20px;
+            }
         }
     }
 </style>
